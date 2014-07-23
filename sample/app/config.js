fa445f976596f4d062afad48578f957e98a0bf7d
John O'Connor <sax1johno@gmail.com>
2014-07-23 19:53:19 -0400

    Updated to c9 architect

--- sample/app/config.js
+++ sample/app/config.js
@@ -1,9 +1,51 @@
 var express = require('express');
 
 // Return the initial application configuration method.
-module.exports = function() {
-    return {
-        "appProvider": express,
-        "port": process.env.PORT,
-    };
-};
+module.exports = [
+    { 
+        packagePath: "./acute",
+        environment: "production",
+    },
+    {
+        packagePath: "./core/app",
+        port: process.env.PORT || 8080,
+        appSettings: {
+            uploadDir: "/public/files",
+            publicdir: "/public",
+            sessionSecretSalt: "keyboardcat",
+            viewsDir: "/views",
+            viewEngine: "jade"
+        }
+    },
+    {
+        packagePath: "./core/data",
+        environment: {
+            production: {
+                data: {
+                    provider: "mongodb",
+                    host: "localhost:1337",
+                    database: "test",
+                    username: "test",
+                    password: "test"
+                },
+                different_data: {
+                    provider: "redis",
+                    host: "localhost:7331",
+                    database: "test",
+                    username: "test",
+                    password: "test"
+                }
+            },
+            development: {
+                data: {
+                    provider: "mongodb",
+                    host: "devserver:3737",
+                    database: "dev",
+                    username: "test",
+                    password: "test"
+                }
+            }
+        }
+    },
+    "./core/controller"
+];
