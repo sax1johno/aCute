c195a2223b900cde06c5b6a509db294ac9ffcdcd
John O'Connor <sax1johno@gmail.com>
2015-01-03 10:17:50 +0000

    Updated the data and models plugins

--- plugins/acute-data-mongodb/lib/index.js
+++ plugins/acute-data-mongodb/lib/index.js
@@ -14,36 +14,41 @@
  * @register is the callback to be called when the plugin is done initializing.
  */
 module.exports = function setup(options, imports, register) {
-  if (!options.environments) {
-    register("No environments were found");
-  } else {
-      var async = require('async');
-      
-      /**
-       * This searches through the current environment configurations and uses the
-       * config info for data from there only.
-       * Providers can be added by installing the "acute-data-*" npm package, where
-       * "*" is the name of the provider.
-       **/
-        register(null, {
-          mongodb: {
-            connect: function(config, fn) {
-                  var mongoose = require('mongoose');
-                  var connectionString = "mongodb://";
-                  if (config.host && config.database) {
-                      if (config.username && config.password) {
-                          connectionString += config.username + ":" + config.password + "@";
-                      }
-                      connectionString += config.host + "/" + config.database;
-                      var connection = mongoose.connect(connectionString);
-                      console.log("database = " + connectionString);
-                      fn(null, connection);
-                  } else {
-                      fn("Unable to get database connection");      
-                  }
-            }
-          }
-        });
-//   var db = imports.database;
+  var async = require('async');
+  
+  var disconnect = function(fn) {
+    var mongoose = require('mongoose');
+    mongoose.disconnect(fn);
   }
+  
+  var connect = function(config, fn) {
+    var mongoose = require('mongoose');
+    var connectionString = "mongodb://";
+    if (config.host && config.database) {
+        if (config.username && config.password) {
+            connectionString += config.username + ":" + config.password + "@";
+        }
+        connectionString += config.host + "/" + config.database;
+        try {
+          var connection = mongoose.connect(connectionString);
+          fn(null, connection);
+        } catch (e) {
+          fn(e);
+        }
+    } else {
+        fn("Unable to get database connection");      
+    }
+  };
+  /**
+   * This searches through the current environment configurations and uses the
+   * config info for data from there only.
+   * Providers can be added by installing the "acute-data-*" npm package, where
+   * "*" is the name of the provider.
+   **/
+    register(null, {
+      'data-mongodb': {
+        connect: connect,
+        disconnect: disconnect
+      }
+    });
 };
--- plugins/acute-express-data/lib/index.js
+++ plugins/acute-express-data/lib/index.js
@@ -3,7 +3,8 @@
  * environment.  Gets the environment from the app.settings.env variable, which itself reads
  * the NODE_ENV environment variable and defaults to "development" if none is present.
  **/
-
+var _ = require('underscore'),
+    sutil = require('util');
 /**
  * @options is the hash of options the user passes in when creating an instance
  * of the plugin.
@@ -11,55 +12,93 @@
  * @register is the callback to be called when the plugin is done initializing.
  */
 module.exports = function setup(options, imports, register) {
-  if (!options.environments) {
-    register("No environments were found");
-  } else {
-      var app = imports.app,
+      var app = imports.app.app,
           async = require('async');
       
       /**
-       * This searches through the current environment configurations and uses the
+       * Searches through the current environment configurations and uses the
        * config info for data from there only.
        * Providers can be added by installing the "acute-data-*" npm package, where
        * "*" is the name of the provider.
+       * @param opts an object of config parameters.
+       * @param fn a callback that takes an error param (err).
        **/
-      async.each(options.environments[app.settings.env].keys(), function(dataConfig, cb) {
-        var configObject = options.environments[app.settings.env][dataConfig];
-        if (!configObject.hasOwnProperty("provider")) {
-          cb("Provider is a required field");
-        } else {
-          var provider = imports["acute-data-" + configObject.provider];
-          if (provider === undefined || provider === null) {
-            cb("Unable to require / find a registered handler for " + configObject.provider);
+      var connect = function(opts, fn) {
+        async.each(_.keys(options.environments[app.get('env')]), function(dataConfig, cb) {
+          var configObject = options.environments[app.get('env')][dataConfig];
+          if (!configObject.hasOwnProperty("provider")) {
+            cb("Provider is a required field");
           } else {
-            var currentData;
-            if (app.get("data") !== undefined && app.get("data") !== null) {
-              currentData = app.get("data");
+            // var provider = imports["acute-data-" + configObject.provider];
+            var provider = imports["data-" + configObject.provider];
+            console.log("Provider = ", provider);
+            if (_.isUndefined(provider) || _.isNull(provider)) {
+              cb("Unable to find a registered handler for " + configObject.provider);
             } else {
-              currentData = {};
-            }
-            // Providers all implement a "connect" method that returns
-            // some form of database connector.
-            provider.connect(configObject.provider, function(err, connection) {
-              if (!err) {
-                currentData[dataConfig] = connection;
-                app.set("data", currentData);
-                cb();
+              var currentData;
+              if (app.get("data") !== undefined && app.get("data") !== null) {
+                currentData = app.get("data");
               } else {
-                cb(err);
+                currentData = {};
               }
-            });
+              // Providers all implement a "connect" method that returns
+              // some form of database connector.
+              // console.log("provider = ", sutil.inspect(provider));
+              provider.connect(configObject, function(err, connection) {
+                if (!err) {
+                  currentData[dataConfig] = {
+                    'provider': provider,
+                    'connection': connection
+                  }
+                  app.set("data", currentData);
+                  cb(null, connection);
+                } else {
+                  cb(err);
+                }
+              });
+            }
           }
-        }
-      }, function(err) {
-        if (err) {
-          register(err);
+        }, function(err) {
+          fn(err);
+        });
+      };
+      
+      var getData = function() {
+        return app.get("data")        
+      };
+      
+      /**
+       * Disconnect a data source.  If no provider is included, the function
+       * disconnects all data sources.
+       * @param provider an optional provider to disconnect
+       * @param fn a callback that's called after disconnecting is complete
+       **/
+      var disconnect = function(provider, fn) {
+        if (!_.isUndefined(provider)) {
+          try {
+            getData()[provider].provider.disconnect(fn);
+          } catch (e) {
+            fn(e);
+          }
         } else {
-          register(null, {
-            data: app.get("data")
+          async.each(_.keys(getData()), function(key, cb) {
+            try {
+              getData()[key].provider.disconnect(fn);
+              cb();
+            } catch (e) {
+              cb(e);
+            }
+          }, function(err) {
+            fn(err);
           });
         }
+      };
+      
+      register(null, {
+        data: {
+          connect: connect,
+          dataSources: getData,
+          disconnect: disconnect
+        }
       });
-//   var db = imports.database;
-  }
 };
