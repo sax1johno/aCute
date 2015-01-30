a75ee23439529e471b9f7c500e5ad53cc9475575
John O'Connor <sax1johno@gmail.com>
2015-01-30 19:27:59 +0000

    Refactored the app boostrap a little to fix an issue with the config running synchronously

--- app/app.js
+++ app/app.js
@@ -4,27 +4,31 @@
  **/
 var architect = require('architect'),
     path = require('path'),
-    configPath = path.join(__dirname, "config.js"),
-    config = architect.loadConfig(configPath);
-
+    configPath = path.join(__dirname, "config.js");
+    
 /**
  * Finally, let's boot up this app and get it running.  Once the application
  * is booted, you can use any mechanism you want to listen on any port
  * you'd like and the application configures the port.
  **/
  
- console.log("Booting app");
- 
- architect.createApp(config, function(err, arch) {
-    if (!err) {
-        /**
-         * Listen on the configured port.
-         **/
-         var appService = arch.getService("app");
-         appService.app.listen(appService.config.port);
-        console.log("Now listeneing to acute app on port ", appService.config.port);
+architect.loadConfig(configPath, function(err, conf) {
+    if (err) {
+        console.error("Unable to boot application:", err);
     } else {
-        // If the boot promise is rejected, then display the errors.
-        console.error("Unable to boot application: ", err);
+        architect.createApp(conf, function(err, arch) {
+            if (!err) {
+                /**
+                 * Listen on the configured port.
+                 **/
+                 var appService = arch.getService("app");
+                 console.log("config = ", appService.appConfig);
+                 appService.app.listen(appService.config.port);
+                console.log("Now listeneing to acute app on port ", appService.config.port);
+            } else {
+                // If the boot promise is rejected, then display the errors.
+                console.error("Unable to boot application: ", err);
+            }
+        });
     }
 });
